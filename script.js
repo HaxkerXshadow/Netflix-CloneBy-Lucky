6880fc569ae44de43c8c2583e8f686f50a984b0a
HaxkerXshadow <rahulsujainwal@gmail.com>
2024-08-02 12:30:11 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,46 @@
+let SVG_Plus1 = document.getElementById('plus1') ;
+let SVG_Cross1 = document.querySelectorAll('cross1') ;
+function click1(){
+   let  Div = document.getElementById('change-div-1');
+   let H4 = document.getElementById('h4-1');
+   Div.classList.toggle('togle');
+   Div.classList.toggle('change');
+   // SVG_Cross1.classList.toggle('svg-cross-ch1');
+   // SVG_Cross1.classList.toggle('svg-cross1');
+   // SVG_Plus1.classList.toggle('svg-plus-ch1');
+   // SVG_Plus1.classList.toggle('svg-plus1');
+   // console.log(SVG_Cross1);
+
+}
+
+function click2(){
+    let  Div = document.getElementById('change-div-2');
+    let H4 = document.getElementById('h4-2');
+    Div.classList.toggle('togle');
+    Div.classList.toggle('change');
+ }
+ function click3(){
+    let  Div = document.getElementById('change-div-3');
+    let H4 = document.getElementById('h4-3');
+    Div.classList.toggle('togle');
+    Div.classList.toggle('change');
+ }
+ function click4(){
+    let  Div = document.getElementById('change-div-4');
+    let H4 = document.getElementById('h4-4');
+    Div.classList.toggle('togle');
+    Div.classList.toggle('change');
+ }
+ function click5(){
+    let  Div = document.getElementById('change-div-5');
+    let H4 = document.getElementById('h4-5');
+    Div.classList.toggle('togle');
+    Div.classList.toggle('change');
+ }
+ function click6(){
+    let  Div = document.getElementById('change-div-6');
+    let H4 = document.getElementById('h4-6');
+    Div.classList.toggle('togle');
+    Div.classList.toggle('change');
+ }
+ 
